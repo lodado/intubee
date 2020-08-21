9e0a3545f67067c04950fdeee651d3cd65a3e8a0
Lee <40421183+lodado@users.noreply.github.com>
2020-08-21 19:06:23 +0900

    slide
    image slide

--- app/src/main/java/com/Introbe/mainpage/frag1.java
+++ app/src/main/java/com/Introbe/mainpage/frag1.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +17,20 @@ import com.Introbe.R;
 public class frag1 extends Fragment {
 
     private photo photoing = new photo();
+    ViewFlipper v_fllipper;
+
+
+    public void fllipperImages(int image) {
+        ImageView imageView = new ImageView(getActivity());
+        imageView.setBackgroundResource(image);
+
+        v_fllipper.addView(imageView);      // 이미지 추가
+        v_fllipper.setFlipInterval(4000);       // 자동 이미지 슬라이드 딜레이시간(1000 당 1초)
+        v_fllipper.setAutoStart(true);          // 자동 시작 유무 설정
+        // animation
+        v_fllipper.setInAnimation(getActivity(),android.R.anim.slide_in_left);
+        v_fllipper.setOutAnimation(getActivity(),android.R.anim.slide_out_right);
+    }
 
     @Nullable
     @Override
@@ -26,11 +42,17 @@ public class frag1 extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        String[] stcArray = getResources().getStringArray(R.array.frag1slide)
+        int images[] = {
+                R.drawable.b1,
+                R.drawable.b2,
+                R.drawable.wallpaper_alena_velichko_08_1920x1200
+        };
 
-        AutoScrollAdapter scrolling = new AutoScrollAdapter(this, srcArray)
+        v_fllipper = getView().findViewById(R.id.image_slide);
 
-
+        for(int image : images) {
+            fllipperImages(image);
+        }
 
     }
 }
